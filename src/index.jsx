8e2d401c967714847af5b707b293d0ccdfc17e20
maria_woland <eqizashvili16@gmail.com>
2023-10-19 02:04:00 +0400

    modified:   src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
 import Footer from './Footer';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Contact() {
+    return <h2>Contact</h2>;
+}
+
+function Apli() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Route path="/about"> <About /> </Route>
+                <Route path="/contact"> <Contact /> </Route>
+                <Route path="/"> <Home /> </Route>
+
+            </div>
+        </Router>
+    );
+}
 
 class App extends React.Component {
     constructor() {
